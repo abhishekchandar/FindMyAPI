55ffa17fc915cf49f0f5d1eca2df8836a1fe000b
abhishekchandar <abhishek.ctos@gmail.com>
2020-05-07 03:59:46 +0530

    Initial setup commit

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+      <h1>Search</h1>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: { }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
